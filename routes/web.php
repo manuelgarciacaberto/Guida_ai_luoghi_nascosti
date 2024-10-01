3febdbeac86b91d9b860b05d0d31c15589bea51f
manuelgarciacaberto <163043108+manuelgarciacaberto@users.noreply.github.com>
2024-10-01 17:39:07 +0200

    PrestoIT - BUNDLING DEGLI ASSETS

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//!                             Homepage
+Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+
